a4fd8ffec5d0df6b2ca88c93041d7de773865051
RakshithM-17 <rakshithm025@gmail.com>
2025-02-26 12:55:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDSflsPsx0G-vjKfrh0MbOZlKwOeKWIan8",
-        authDomain: "seller-app-f5e0d.firebaseapp.com",
-        projectId: "seller-app-f5e0d",
-        storageBucket: "gs://seller-app-f5e0d.appspot.com",
-        messagingSenderId: "663355668762",
-        appId: "1:663355668762:android:8292b459e67c12425d54a3"
-};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
